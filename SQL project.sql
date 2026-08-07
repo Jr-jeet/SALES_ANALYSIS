d925430cbf870eb10ffaa032691be9e2065be1cc
Jr-jeet <jeet62032@gmail.com>
2026-08-07 23:43:11 +0530

    HERE IS THE MORE QUERY FOR KNOWING SOME INFORMATION
    Added multiple aggregate queries to analyze sales data by various dimensions.

--- SQL project.sql
+++ SQL project.sql
@@ -5,3 +5,29 @@ from xyz.'sales dataset'
 group by category 
 order by total_profit DESC;
 show tables
+  
+select avg(profit)
+from xyz.`sales dataset`;
+
+select count(*),State
+from xyz.`sales dataset`
+group by State
+order by count(State)DESC;
+
+select sum(Profit),State
+from xyz.`sales dataset`
+group by State
+order by count(Profit) DESC;
+
+select count(*),CustomerName
+from xyz.`sales dataset`
+group by CustomerName
+order by count(*) DESC;
+
+select count(*),CustomerName
+from xyz.`sales dataset`
+group by CustomerName
+order by count(*) DESC;
+
+select max(Profit),Sub-Category
+from xyz.`sales dataset`;
